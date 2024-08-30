9d07d61d51bf124944065c8d980fe76739b14f0d
YINOQI <1028@yinoqi.com>
2024-08-30 13:10:15 +0800

    加入redisson

--- src/main/java/com/lwl/social_media_platform/utils/RedisConstant.java
+++ src/main/java/com/lwl/social_media_platform/utils/RedisConstant.java
@@ -3,6 +3,9 @@ package com.lwl.social_media_platform.utils;
 public class RedisConstant {
     public static final String USER_KEY = "user:";
     public static final String USER_LOGIN_KEY = "user:login:";
-    public static final String TREADS_VO_KEY = "treads:vo";
+    public static final String TREADS_VO_KEY = "treads:vo:";
+    public static final String SUPPORT_KEY = "support:entity";
+    public static final String SUPPORT_NUM_KEY = "support:num";
+    public static final String SUPPORT_LOCK = "support:lock";
     public static final Long USER_TTL = 3L;
 }
